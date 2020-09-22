4c1319eb20a9cd46a2931399522b6a36c637511f
rfaridi <rushad.16@gmail.com>
2020-09-22 14:28:49 +0600

    first draft

--- Resources/onion-price/onion-price-script.R
+++ Resources/onion-price/onion-price-script.R
@@ -1,29 +1,36 @@
 library(tidyverse)
 library(magick)
 
-price.image.list <- list.files(./Re
+dir <- "./Resources/onion-price/"
 
-onion.prices.local <- list.files("./Resources/onion-price/", pattern="local")
-onion.prices.imported <- list.files("./Resources/onion-price/", pattern="imported")
+onion.prices.local <- list.files(dir, pattern="local")
+onion.prices.import <- list.files(dir, pattern="imported")
 
-local.image <- map(onion.prices.local, image_read) 
-imported <- map(onion.prices.imported, image_read) 
+local.onion <- map(glue::glue("{dir}/{onion.prices.local}"), image_read) 
+import.onion <- map(glue::glue("{dir}/{onion.prices.import}"), image_read) 
 
-local.image %>% 
-    as.character()
-    image_append(stack=TRUE) %>% 
-    image_display()
-
-local.image.vec <- local.image[[1]] 
-for(i in 2:length(local.image)){
-  local.image.vec <- c(local.image.vec ,local.image[[i]] )
+local.onion.vec <- local.onion[[1]] 
+for(i in 2:length(local.onion)){
+  local.onion.vec <- c(local.onion.vec ,local.onion[[i]] )
 }
 
-local.image.vec1 <- c(local.image[[1]],local.image[[2]],local.image[[3]],
-  local.image[[4]])
+local.image.vec1 <- local.onion.vec[c(1:4)]
 
 local.image.vec2  <- c(local.image[[5]],local.image[[6]],
   local.image[[7]],local.image[[8]])
+
+local.onion.vec %>% 
+    image_scale("300") %>% 
+    image_montage()  %>% 
+    image_display()
+
+local.onion.vec %>% 
+    image_scale("500") %>% 
+    image_composite()  %>% 
+    image_display()
+
+
+
 
 local.image.vec1 %>% 
     image_scale("500") %>% 
